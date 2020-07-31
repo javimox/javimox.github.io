382ece390a42f08095114fa193c161d07f412a59
jmox <mox@mox.sh>
2020-07-31 20:15:20 +0200

    added cookie consent and terms and privacy policy page

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,14 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#383b75"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "href": "https://mox.sh/terms"
+  }
+});
